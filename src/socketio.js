07dde11b7e52fdfe0e0df69b075f9aeb39c514dc
jooyeon Lee <ljyeon0713@naver.com>
2023-09-17 21:29:35 +0900

    feat: 연결 끊긴 유저 게임 내 제거 기능 구현

--- src/socketio.js
+++ src/socketio.js
@@ -1,134 +1,137 @@
-import { io } from 'socket.io-client';
-import axiosClient from './utils/axiosClient';
-import { meActions } from './store/modules/me';
-import GetMyData from './hoc/GetMyData';
-import { usersActions } from './store/modules/users';
-import { store } from './store/store';
-import { gameInfoActions } from './store/modules/gameInfo';
+import { io } from "socket.io-client";
+import axiosClient from "./utils/axiosClient";
+import { meActions } from "./store/modules/me";
+import GetMyData from "./hoc/GetMyData";
+import { usersActions } from "./store/modules/users";
+import { store } from "./store/store";
+import { gameInfoActions } from "./store/modules/gameInfo";
 
 export let socket;
 //소켓 연결
 export const initSocketConnection = () => {
-  if (socket) return;
-  socket = io(`${process.env.REACT_APP_API_URL}`, {
-    transports: ['websocket'],
-  });
-  socket.connect();
-  socket.on('disconnect', () => {
-    alert('연결이 종료되었습니다.');
-    window.location.href = '/';
-  });
-  socket.on('updateNickname', (id, nickname) => {
-    console.log(id, store.getState().me.id);
-    if (id == store.getState().me.id) return;
-    store.dispatch(usersActions.updateNickname({ id, nickname }));
-  });
-  socket.on('updateLocation', (id, top, left) => {
-    if (id == store.getState().me.id) return;
-    store.dispatch(usersActions.updateLocation({ id, top, left }));
-  });
-  socket.on('ready', (id, isReady) => {
-    if (id == store.getState().me.id) return;
-    store.dispatch(usersActions.ready({ id, isReady }));
-  });
-  socket.on('gameStart', (data) => {
-    const myData = store.getState().me;
-    let myNewData;
-    let userData = data.filter((user) => {
-      if (user[0] == myData.id) myNewData = user;
-      return user[0] != myData.id;
+    if (socket) return;
+    socket = io(`${process.env.REACT_APP_API_URL}`, {
+        transports: ["websocket"],
     });
-    store.dispatch(
-      meActions.location({
-        top: myNewData[1].top,
-        left: myNewData[1].left,
-      }),
-    );
-    store.dispatch(usersActions.join(userData));
-    store.dispatch(gameInfoActions.gameStart());
-  });
-  socket.on('hit', (id, data) => {
-    const myData = store.getState().me;
-    if (id == myData.id) store.dispatch(meActions.attack(true));
-    let getHitUsers = data;
-    if (getHitUsers.includes(myData.id)) {
-      store.dispatch(meActions.getHit());
-      getHitUsers = getHitUsers.filter((id) => id !== myData.id);
-    }
-    store.dispatch(usersActions.getHit({ id, getHitUsers }));
-  });
-  socket.on('gameEnd', (wonTeam) => {
-    console.log('gameEnd', wonTeam);
-    store.dispatch(gameInfoActions.gameEnd(wonTeam));
-    store.dispatch(usersActions.gameEnd());
-    store.dispatch(meActions.gameEnd());
-  });
+    socket.connect();
+    socket.on("disconnect", () => {
+        alert("연결이 종료되었습니다.");
+        window.location.href = "/";
+    });
+    socket.on("updateNickname", (id, nickname) => {
+        console.log(id, store.getState().me.id);
+        if (id == store.getState().me.id) return;
+        store.dispatch(usersActions.updateNickname({ id, nickname }));
+    });
+    socket.on("updateLocation", (id, top, left) => {
+        if (id == store.getState().me.id) return;
+        store.dispatch(usersActions.updateLocation({ id, top, left }));
+    });
+    socket.on("ready", (id, isReady) => {
+        if (id == store.getState().me.id) return;
+        store.dispatch(usersActions.ready({ id, isReady }));
+    });
+    socket.on("gameStart", (data) => {
+        const myData = store.getState().me;
+        let myNewData;
+        let userData = data.filter((user) => {
+            if (user[0] == myData.id) myNewData = user;
+            return user[0] != myData.id;
+        });
+        store.dispatch(
+            meActions.location({
+                top: myNewData[1].top,
+                left: myNewData[1].left,
+            })
+        );
+        store.dispatch(usersActions.join(userData));
+        store.dispatch(gameInfoActions.gameStart());
+    });
+    socket.on("hit", (id, data) => {
+        const myData = store.getState().me;
+        if (id == myData.id) store.dispatch(meActions.attack(true));
+        let getHitUsers = data;
+        if (getHitUsers.includes(myData.id)) {
+            store.dispatch(meActions.getHit());
+            getHitUsers = getHitUsers.filter((id) => id !== myData.id);
+        }
+        store.dispatch(usersActions.getHit({ id, getHitUsers }));
+    });
+    socket.on("gameEnd", (wonTeam) => {
+        console.log("gameEnd", wonTeam);
+        store.dispatch(gameInfoActions.gameEnd(wonTeam));
+        store.dispatch(usersActions.gameEnd());
+        store.dispatch(meActions.gameEnd());
+    });
+    socket.on("goOut", (userId) => {
+        store.dispatch(usersActions.goOut(userId));
+    });
 };
 
 //채널 입장, 개설 및 해당 채널로 재연결
 export const joinRoom = (roomNumber, dispatch) => {
-  socket.emit('joinRoom', roomNumber);
-  socket.on('joinSuccess', (id, data) => {
-    //id는 joinSuccess를 발생시킨 유저의 id, data는 전체 유저의 데이터
-    const myData = store.getState().me;
-    let myId = '';
-    if (!myData.id) {
-      //사용자 첫 입장시
-      dispatch(
-        meActions.update({
-          id,
-          nickname: '',
-          data: data.filter((user) => user[0] == id)[0][1],
-        }),
-      );
-      myId = id;
-    }
-    dispatch(
-      usersActions.join(
-        data.filter((user) => user[0] !== myId && user[0] !== myData.id), //내가 아닌 다른 유저들의 정보만 유저 상태에 저장
-      ),
-    );
-  });
+    socket.emit("joinRoom", roomNumber);
+    socket.on("joinSuccess", (id, data) => {
+        //id는 joinSuccess를 발생시킨 유저의 id, data는 전체 유저의 데이터
+        const myData = store.getState().me;
+        let myId = "";
+        if (!myData.id) {
+            //사용자 첫 입장시
+            dispatch(
+                meActions.update({
+                    id,
+                    nickname: "",
+                    data: data.filter((user) => user[0] == id)[0][1],
+                })
+            );
+            myId = id;
+        }
+        dispatch(
+            usersActions.join(
+                data.filter((user) => user[0] !== myId && user[0] !== myData.id) //내가 아닌 다른 유저들의 정보만 유저 상태에 저장
+            )
+        );
+    });
 };
 
 export const sendChat = (id, chat) => {
-  socket.emit('chat', id, chat);
+    socket.emit("chat", id, chat);
 };
 
 export const getChat = (setChat) => {
-  socket.on('chat', (id, chat) => {
-    setChat((prev) => [...prev, { id, chat }]);
-  });
+    socket.on("chat", (id, chat) => {
+        setChat((prev) => [...prev, { id, chat }]);
+    });
 };
 export const setNickname = (id, nickname) => {
-  socket.emit('setNickname', id, nickname);
+    socket.emit("setNickname", id, nickname);
 };
 
 export const setLocation = () => {
-  const myData = store.getState().me;
-  socket.emit('setLocation', myData.id, {
-    top: myData.top,
-    left: myData.left,
-  });
+    const myData = store.getState().me;
+    socket.emit("setLocation", myData.id, {
+        top: myData.top,
+        left: myData.left,
+    });
 };
 
 export const attack = () => {
-  const myData = store.getState().me;
-  if (myData.attacked || myData.health <= 0) return;
-  socket.emit('attack', myData.id);
+    const myData = store.getState().me;
+    if (myData.attacked || myData.health <= 0) return;
+    socket.emit("attack", myData.id);
 };
 
 export const ready = () => {
-  const myData = store.getState().me;
-  console.log(myData);
-  socket.emit('ready', myData.id, myData.isReady);
+    const myData = store.getState().me;
+    console.log(myData);
+    socket.emit("ready", myData.id, myData.isReady);
 };
 
 // 소켓 연결을 끊음
 export const disconnectSocket = () => {
-  if (socket == null || socket.connected === false) {
-    return;
-  }
-  socket.disconnect();
-  socket = undefined;
+    if (socket == null || socket.connected === false) {
+        return;
+    }
+    socket.disconnect();
+    socket = undefined;
 };
--- src/store/modules/gameInfo.ts
+++ src/store/modules/gameInfo.ts
@@ -1,26 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  isGaming: false,
-  isMovable: true,
-  lastWonTeam: '',
+    isGaming: false,
+    isMovable: true,
+    winTeam: "",
 };
 const gameInfo = createSlice({
-  initialState,
-  name: 'gameInfo',
-  reducers: {
-    gameStart: (state) => {
-      state.isGaming = true;
-      state.isMovable = false;
+    initialState,
+    name: "gameInfo",
+    reducers: {
+        gameStart: (state) => {
+            state.isGaming = true;
+            state.isMovable = false;
+        },
+        gameEnd: (state, action) => {
+            state.isGaming = false;
+            state.winTeam = action.payload;
+        },
+        isMovable: (state) => {
+            state.isMovable = true;
+        },
+        resetWinTeam: (state) => {
+            state.winTeam = "";
+        },
     },
-    gameEnd: (state, action) => {
-      state.isGaming = false;
-      state.lastWonTeam = action.payload;
-    },
-    isMovable: (state) => {
-      state.isMovable = true;
-    },
-  },
 });
 export const gameInfoActions = gameInfo.actions;
 export default gameInfo.reducer;
